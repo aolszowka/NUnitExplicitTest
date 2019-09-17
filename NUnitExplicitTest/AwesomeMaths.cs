9aadb29d5df284dda6f4d503549b45f53172495a
Per Stolpe <perstolpe@gmail.com>
2019-09-17 11:19:01 +0200

    stuff that does not work as i think it should

---
+++ NUnitExplicitTest/AwesomeMaths.cs
@@ -0,0 +1,8 @@
+﻿namespace NUnitExplicitTest
+{
+    public static class AwesomeMaths
+    {
+        public static int Add(int x, int y) => x + y;
+        public static double Divide(int x, int y) => x / y;
+    }
+}
---
+++ Tests/AwesomeMathsTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using NUnitExplicitTest;
+
+namespace Tests
+{
+    public class AwesomeMathsTests
+    {
+        [Test]
+        public void AddWorks()
+        {
+            Assert.AreEqual(3, AwesomeMaths.Add(1, 2));
+        }
+
+        [Test, Explicit]
+        public void DivideDoesntWork()
+        {
+            Assert.AreEqual(1337, AwesomeMaths.Divide(42, 0));
+        }
+    }
+}
