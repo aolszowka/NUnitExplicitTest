2b58ec874f9fdbf0463a195f739c7f059f17eadb
Per Stolpe <perstolpe@gmail.com>
2019-09-17 11:57:56 +0200

    minimal example

--- NUnitExplicitTest/AwesomeMaths.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace NUnitExplicitTest
-{
-    public static class AwesomeMaths
-    {
-        public static int Add(int x, int y) => x + y;
-        public static double Divide(int x, int y) => x / y;
-    }
-}
---
+++ Tests/NUnitTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Tests
+{
+    public class NUnitTests
+    {
+        [Test]
+        public void TestThatWorks()
+        { }
+
+        [Test, Ignore("Does not work")]
+        public void IgnoredTestDoesntWork()
+        {
+            throw new Exception();
+        }
+
+        [Test, Explicit]
+        public void ExplicitTestDoesntWork()
+        {
+            throw new Exception();
+        }
+    }
+}
